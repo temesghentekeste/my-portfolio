5f9f2686b568d38dab6c1d6faf0f78f48b42fa86
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-12-21 14:05:51 +0300

    Add contact from and submit form data using formspree

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,32 @@ import React from "react"
 import Layout from "../components/Layout"
 
 const contact = () => {
-  return <h2>contact page</h2>
+  return <Layout>
+      <section className="contact-page">
+        <article className="contact-form">
+          <h3>get in touch</h3>
+          <form action="https://formspree.io/f/xvovvgnk" method="POST">
+            <div className="form-group">
+              <input 
+                type="text"
+                name="name" 
+                placeholder="name" 
+                className="form-control"/>
+              <input 
+                type="email"
+                name="email" 
+                placeholder="email" 
+                className="form-control"/>
+              <textarea name="message" placeholder="messae" rows='10' className="form-control"/>
+              <button type="submit" className="submit-btn btn">
+                submit
+              </button>
+
+            </div>
+          </form>
+        </article>
+      </section>
+    </Layout>
 }
 
 export default contact
